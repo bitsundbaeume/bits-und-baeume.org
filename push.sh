8dca412ab1a5396ed92820ec2fc4592cf8861775
b3nks <github@postelio.de>
2025-01-14 15:40:48 +0100

    show log forever

--- push.sh
+++ push.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-URL="https://bubweb.uber.space/bitbaumweb_build_main.log"
 
 git add --all
 #if [ $? -eq 0 ]; then echo "nichts zu tun"; exit; fi
@@ -7,7 +6,6 @@ git status
 echo "Commitname:"
 read beschreibung
 git commit -m "$beschreibung"
-sleep
 git push
 sleep 5
-watch -d -n 2 'curl -s $URL | tail -n 5'
+watch -d -n 2 'curl -s https://bubweb.uber.space/bitbaumweb_build_main.log | tail -n 5'
