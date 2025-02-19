08b1955e4191459b64df54ac424325630bf7f775
b3nks <github@postelio.de>
2025-02-19 18:01:52 +0100

    Serve Script mit Logs und ignores

--- serve.sh
+++ serve.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 cd "$(dirname "$0")"
-npx @11ty/eleventy --serve
+echo "B&B Website wird lokal mit 11ty bereit gestellt:"
+echo "################################################"
+npx @11ty/eleventy --serve | tee 11ty-latest-serve.log
