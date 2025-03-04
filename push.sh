e490e5d1b2504f215d91b496c30d7166b3ac9a75
b3nks <github@postelio.de>
2025-03-04 15:01:05 +0100

    push script ohne commit

--- push.sh
+++ push.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-git add --all
+#git add --all
 #if [ $? -eq 0 ]; then echo "nichts zu tun"; exit; fi
-git status
-echo "Commitname:"
-read beschreibung
-git commit -m "$beschreibung"
+#git status
+#echo "Commitname:"
+#read beschreibung
+#git commit -m "$beschreibung"
 git push
 sleep 5
 watch -d -n 2 'curl -s https://bubweb.uber.space/bitbaumweb_build_main.log | tail -n 5'
